12fa4d1261542a98d4e16ffad3d920817044f6d9
Jemal <jemal@zoomdata.com>
2017-11-28 19:38:33 -0500

    priority num change

--- src/test/java/com/zoomdataJemal/smokeTests/LoginTest.java
+++ src/test/java/com/zoomdataJemal/smokeTests/LoginTest.java
@@ -9,17 +9,17 @@ import sun.jvm.hotspot.debugger.Page;
 public class LoginTest extends BaseClass{
 
 
-@Test(priority = 1)
+@Test(priority = 0)
     public void canGoToLoginPage(){
                   Pages.loginPage().goToLoginPage();
                   Assert.assertTrue(Pages.loginPage().isAtLoginPage());
 }
-@Test(priority = 2)
+@Test(priority = 1)
     public void loginAsValidZoomDataUser() throws InterruptedException {
                   Pages.loginPage().loginAsValidUser("admin","Z00mda1a!");
 
 }
-@Test(priority = 3)
+@Test(priority = 2)
     public void canGoToSourcePage() throws InterruptedException {
                   Pages.sourcePage().goToSource();
                  // Pages.loginPage().loginAsValidUser("admin","Z00mda1a!");
